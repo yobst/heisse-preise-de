b4b13801890ee892ce40aa48b49c515bacc9d818
Mario Zechner <badlogicgames@gmail.com>
2023-09-25 20:57:29 +0200

    Remove unnecessary stuff.

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-CMD="cd heissepreise && git checkout -- . && ./docker/control.sh stop && ./docker/control.sh start && ./docker/control.sh logs"
-echo $CMD
-ssh -t marioslab.io $CMD
