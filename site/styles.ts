19dc6653a2f29820d2564e9685d11e6f7b16b641
Timo Kramer <4785848+TimoKramer@users.noreply.github.com>
2023-11-20 17:13:05 +0100

    feat: store data in postgresql (#10)

--- site/styles.ts
+++
@@ -1,7 +0,0 @@
-import { unsafeCSS } from "lit";
-
-import "./styles-bundle.css";
-// @ts-ignore
-import globalCssTxt from "./styles-bundle.css.txt";
-
-export const globalStyles = [unsafeCSS(globalCssTxt)];
