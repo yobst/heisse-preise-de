9a4621d32e9ebd2636b68d3585ba1c3fb09d1cd4
JMassa <j.massa@lambdaforge.io>
2023-12-18 19:28:13 +0100

    feat: fetch categories

--- server/crawlers/aldi.ts
+++ server/crawlers/aldi.ts
@@ -5,6 +5,9 @@ import get from "axios";
 import * as utils from "./utils";
 import { stores } from "../../common/stores";
 
+const BASE_URL = "https://api.de.prod.commerce.ci-aldi.com/v1/catalog-search-product-offers";
+const MERCHANT = "ADG045_1";
+
 const storeUnits: Record<string, UnitMapping> = {
     ea: { unit: "stk", factor: 1 },
     "er-set": { unit: "stk", factor: 1 },
@@ -13,21 +16,59 @@ const storeUnits: Record<string, UnitMapping> = {
 
 const invalidUnits = new Set(["v", "-lagig", "klingen", "-klingen"]);
 
+function getSubcategories(category: any) {
+    let categories = [
+        {
+            name: category.name,
+            urlSlugText: category.urlSlugText,
+            id: category.nodeId,
+            categoryKey: category.categoryKey,
+            code: null,
+        },
+    ];
+    for (const subcategory of category.children) {
+        categories = categories.concat(getSubcategories(subcategory));
+    }
+
+    return categories;
+}
+
 export class AldiCrawler implements Crawler {
     store = stores.aldi;
 
+    async fetchCategories() {
+        const pageLimit = 12; // lowest possible number; allowed are 12, 16, 24, 30, 32, 48
+        let categories: Record<number, any> = {};
+        const ALDI_ITEM_SEARCH = `${BASE_URL}?page[limit]=${pageLimit}&page[offset]=0&merchantReference=${MERCHANT}`;
+        const resp = (await get(ALDI_ITEM_SEARCH)).data;
+
+        for (const category of resp.data[0].attributes.categoryTreeFilter) {
+            if (!(category.nodeId in categories)) {
+                const subcategories = getSubcategories(category);
+                for (const subcategory of subcategories) {
+                    categories[subcategory.id] = subcategory;
+                }
+            }
+        }
+        console.log(`Found ${Object.keys(categories).length} categories for Aldi`);
+        console.log(Object.values(categories));
+
+        return Object.values(categories);
+    }
+
     async fetchData() {
+        const pageLimit = 48;
         let offset = 0;
         let items: any[] = [];
         let done = false;
         while (!done) {
-            const ALDI_ITEM_SEARCH = `https://api.de.prod.commerce.ci-aldi.com/v1/catalog-search-product-offers?page[limit]=48&page[offset]=${offset}&merchantReference=ADG045_1`;
+            const ALDI_ITEM_SEARCH = `${BASE_URL}?page[limit]=${pageLimit}&page[offset]=${offset}&merchantReference=${MERCHANT}`;
             const resp = (await get(ALDI_ITEM_SEARCH)).data;
             const maxpage = resp.data[0].attributes.pagination.maxPage;
             const currentpage = resp.data[0].attributes.pagination.currentPage;
             done = offset > 5000 || currentpage >= maxpage;
             items = items.concat(resp.data[0].attributes.catalogSearchProductOfferResults);
-            offset += 48;
+            offset += pageLimit;
         }
         return items;
     }
