f88c68039bc7f93b0b0c019f9976146ee9f323f8
JMassa <j.massa@lambdaforge.io>
2023-12-18 19:29:18 +0100

    feat: general fetch categories interface

--- server/crawlers/crawler.ts
+++ server/crawlers/crawler.ts
@@ -2,6 +2,7 @@ import { Category, Item, Store } from "../../common/models";
 
 export interface Crawler {
     store: Store;
+    fetchCategories(): Promise<any[]>;
     fetchData(): Promise<any[]>;
     getCanonical(rawItem: any, today: string): Item;
     getCategory(rawItem: any): Category;
