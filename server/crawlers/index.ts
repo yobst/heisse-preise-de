16d982c7b61a2570f9728bb913f605f383009489
JMassa <j.massa@lambdaforge.io>
2023-12-21 05:32:07 +0100

    feat: add biomarkt

--- server/crawlers/index.ts
+++ server/crawlers/index.ts
@@ -5,8 +5,17 @@ import { FlinkCrawler } from "./flink";
 import { LidlCrawler } from "./lidl";
 import { MuellerCrawler } from "./mueller";
 import { ReweCrawler } from "./rewe";
+import { BiomarktCrawler } from "./biomarkt";
 
-const crawlerList = [new AldiCrawler(), new DmCrawler(), new LidlCrawler(), new MuellerCrawler(), new ReweCrawler(), new FlinkCrawler()];
+const crawlerList = [
+    new AldiCrawler(),
+    new DmCrawler(),
+    new LidlCrawler(),
+    new MuellerCrawler(),
+    new ReweCrawler(),
+    new FlinkCrawler(),
+    new BiomarktCrawler(),
+];
 
 export const crawlers: Record<string, Crawler> = {};
 crawlerList.forEach((crawler) => (crawlers[crawler.store.id] = crawler));
