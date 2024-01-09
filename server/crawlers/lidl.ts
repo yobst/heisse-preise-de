4ba23c1e9f3a6fa76062d762f84d6f067af3bfbe
Judith <judith.massa@yobst.de>
2024-01-09 17:53:41 +0100

    test: add lidl tests

--- server/crawlers/lidl.ts
+++ server/crawlers/lidl.ts
@@ -1,9 +1,11 @@
-import { Category, Item, Unit, UnitMapping } from "../../common/models";
+import { Item, Unit, UnitMapping } from "../../common/models";
 import { Crawler } from "./crawler";
 
 import get from "axios";
 import * as utils from "./utils";
 import { stores } from "../../common/stores";
+
+const BASE_URL = "https://www.lidl.de/p/api/gridboxes/DE/de";
 const HITS = Math.floor(30000 + Math.random() * 2000);
 
 const storeUnits: Record<string, UnitMapping> = {
@@ -20,13 +22,43 @@ const storeUnits: Record<string, UnitMapping> = {
     m2: { unit: "qm", factor: 1 },
 };
 
+export function getQuantityAndUnit(rawItem: any, storeName: string) {
+    const defaultUnit: { quantity: number; unit: Unit } = { quantity: 1, unit: "stk" };
+
+    let rawQuantity = rawItem.price.packaging?.amount;
+    let rawUnit = rawItem.price.packaging?.unit;
+    console.log("1:", rawQuantity, " ", rawUnit);
+    
+    if (!rawUnit && rawItem.price.packaging?.text) {
+        const res = utils.extractRawUnitAndQuantityFromEndOfString(rawItem.price.packaging.text, defaultUnit);
+        rawQuantity = res.rawQuantity;
+        rawUnit = res.rawUnit;
+    }
+    console.log("2:", rawQuantity, " ", rawUnit);
+
+    if (!rawUnit) {
+        const description = `${rawItem.keyfacts?.supplementalDescription?.concat(" ") ?? ""}${rawItem.fullTitle}`;
+        const regex = /([0-9]+)\s+Stück/;
+        const matches = description.match(regex);
+        rawUnit = "stk";
+        rawQuantity = 1;
+        if (matches) {
+            rawQuantity = parseFloat(matches[1]);
+        }
+    }
+    console.log("3:", rawQuantity, " ", rawUnit);
+
+    return utils.normalizeUnitAndQuantity(
+        rawItem.fullTitle, rawUnit, rawQuantity, storeUnits, storeName, defaultUnit);
+}
+
 export class LidlCrawler implements Crawler {
     store = stores.lidl;
     categories: Record<string, any> = {};
 
     async fetchCategories() {
-        const LIDL_SEARCH = `https://www.lidl.de/p/api/gridboxes/DE/de/?max=${HITS}`;
-        const data = (await get(LIDL_SEARCH)).data || [];
+        const page = `${BASE_URL}/?max=${HITS}`;
+        const data = (await get(page)).data || [];
         const uniqueCategories: Set<string> = new Set(data.map((item: any) => item.category).filter((item: any) => item));
 
         const categories: Record<string, any> = {};
@@ -42,8 +74,8 @@ export class LidlCrawler implements Crawler {
     }
 
     async fetchData() {
-        const LIDL_SEARCH = `https://www.lidl.de/p/api/gridboxes/DE/de/?max=${HITS}`;
-        const data = (await get(LIDL_SEARCH)).data || [];
+        const page = `${BASE_URL}/?max=${HITS}`;
+        const data = (await get(page)).data || [];
         return data.filter((item: any) => !!item.price.price && item.category 
         && ( item.category.startsWith("Kategorien/Lebensmittel") || item.category.startsWith("Food") )
         );
@@ -51,30 +83,15 @@ export class LidlCrawler implements Crawler {
 
     getCanonical(rawItem: any, today: string): Item {
         const price = rawItem.price.price;
-        const description = `${rawItem.keyfacts?.supplementalDescription?.concat(" ") ?? ""}${rawItem.fullTitle}`;
+        const description = `${rawItem.keyfacts?.supplementalDescription?.concat(" ") ?? ""} ${rawItem.fullTitle}`;
         const itemName = rawItem.fullTitle;
         const bio = description.toLowerCase().includes("bio");
         const unavailable = rawItem.stockAvailability.availabilityIndicator == 0;
         const productId = rawItem.productId;
         const rawCategory = rawItem.category;
         const category: Record<any, any> = this.categories[rawCategory] || "Unknown";
-        const defaultUnit: { quantity: number; unit: Unit } = { quantity: 1, unit: "stk" };
-
-        let isWeighted = false;
-        let rawQuantity = rawItem.price.packaging?.amount;
-        let rawUnit = rawItem.price.packaging?.unit;
-
-        if (!rawUnit) {
-            rawUnit = "stk";
-            rawQuantity = 1;
-            const regex = /([0-9]+)\s+Stück/;
-            const matches = description.match(regex);
-            if (matches) {
-                rawQuantity = parseFloat(matches[1]);
-            }
-        }
-
-        const unitAndQuantity = utils.normalizeUnitAndQuantity(description, rawUnit, rawQuantity, storeUnits, this.store.displayName, defaultUnit);
+        const isWeighted = false;
+        const { quantity, unit } = getQuantityAndUnit(rawItem, this.store.displayName); 
 
         return new Item(
             this.store.id,
@@ -85,8 +102,8 @@ export class LidlCrawler implements Crawler {
             price,
             [{ date: today, price: price, unitPrice: 0 }],
             isWeighted,
-            unitAndQuantity.unit,
-            unitAndQuantity.quantity,
+            unit,
+            quantity,
             bio,
             rawItem.canonicalUrl
         );
